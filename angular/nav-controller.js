af673008fc95e06acb807ded5a60ed5cb07aa0c6
Dylan McDonald <dmcdonald21@cnm.edu>
2016-03-08 21:23:10 -0700

    Added directive to conditionally apply variable based on Bootstrap breakpoints

--- angular/nav-controller.js
+++ angular/nav-controller.js
@@ -1,6 +1,7 @@
 app.controller("NavController", ["$http", "$scope", function($http, $scope) {
+	$scope.breakpoint = null;
+	$scope.navCollapsed = null;
 	$scope.pages = [];
-	$scope.navCollapsed = true;
 
 	$scope.getPages = function() {
 		$http.get("navmap.json")
@@ -14,4 +15,9 @@ app.controller("NavController", ["$http", "$scope", function($http, $scope) {
 	if($scope.pages.length === 0) {
 		$scope.getPages();
 	}
+
+	// collapse the navbar if the screen is changed to a extra small screen
+	$scope.$watch("breakpoint", function() {
+		$scope.navCollapsed = ($scope.breakpoint === "xs");
+	});
 }]);
